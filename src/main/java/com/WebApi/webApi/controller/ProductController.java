64441f1e292993b3ff6635e157c38a2054aa01c1
Mojtaba Delju <mojtabadelju@gmail.com>
2024-09-24 16:04:41 +0300

    product model change

--- src/main/java/com/WebApi/webApi/controller/ProductController.java
+++ src/main/java/com/WebApi/webApi/controller/ProductController.java
@@ -14,8 +14,10 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
+@RequestMapping("/api")
 public class ProductController {
 
     @Autowired
--- src/main/java/com/WebApi/webApi/model/Product.java
+++ src/main/java/com/WebApi/webApi/model/Product.java
@@ -1,5 +1,6 @@
 package com.WebApi.webApi.model;
 
+import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
 import org.hibernate.annotations.CreationTimestamp;
@@ -12,27 +13,47 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 @Entity
 public class Product {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+
+    @Column(nullable = true, columnDefinition = "varchar(255) default 'Default Name'")
     private String name;
+
+    @Column(nullable = true, columnDefinition = "varchar(255) default 'Default Description'")
     private String description;
-    private double price;
+
+    @Column(nullable = false, columnDefinition = "decimal(10,2) default 0.0")
+    private BigDecimal price;
+
+    @Column(nullable = false, columnDefinition = "int default 1")
+    private int quantity;
+
+    @Column(nullable = true, columnDefinition = "varchar(255) default 'default.png'")
+    private String image;
+
+    @Column(nullable = true, columnDefinition = "varchar(255) default 'Uncategorized'")
+    private String category;
+
+    @Column(nullable = true, columnDefinition = "varchar(255) default 'Generic'")
+    private String brand;
+
+    @Column(nullable = false, columnDefinition = "boolean default false")
+    private Boolean available;
+
     @CreationTimestamp
     @Column(updatable = false)
     private LocalDateTime createdAt;
 
     @UpdateTimestamp
     private LocalDateTime updatedAt;
-
-    // Default constructor
-    public Product() {
-    }
 
 }
